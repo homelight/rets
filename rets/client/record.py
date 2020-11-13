d5a71bdc1fe1d6ceb0e6861d917a8cec72f7f109
Blake Embrey <hello@blakeembrey.com>
2020-11-13 10:29:06 -0800

    Delete resource key from record (#56)

--- rets/client/record.py
+++ rets/client/record.py
@@ -6,7 +6,6 @@ from rets.http import Object
 class Record:
 
     def __init__(self, resource_class, data: dict):
-        self.resource = resource_class.resource
         self.resource_class = resource_class
         self.data = data
         
@@ -15,13 +14,8 @@ class Record:
         except KeyError:
             self.resource_key = None
 
-    def get_objects(self, name: str, **kwargs) -> Sequence[Object]:
-        resource_object = self.resource.get_object_type(name)
-        return resource_object.get(self.resource_key, **kwargs)
-
     def __repr__(self) -> str:
-        return '<Record: %s:%s:%s>' % (
+        return '<Record: %s:%s>' % (
             self.resource_class.resource.name,
             self.resource_class.name,
-            self.resource_key,
         )
